a73a7f941622fae51d07149685046bdfb99c764a
JuanmaZimm <juanmanuelzimmermann@gmail.com>
2022-09-05 22:58:55 -0300

    Paso 7 Participante A

--- archivoA1.js
+++ archivoA1.js
@@ -1,2 +1,4 @@
 console.log("You want me, I want you, baby, My sugarboo, I'm levitating,The Milky Way, we're renegading, Yeah, yeah, yeah, yeah, yeah");
 console.log("Juan Manuel Zimmermann");
+
+console.log("Paso 7 Participante A");
